385b5854f05cc568f8b1ad823f8cb6d03451467c
Jutianfu <2652319564@qq.com>
2025-03-07 14:37:05 +0800

    init:项目初始化

---
+++ umps/umps-biz/src/main/java/com/mengxj/ump/biz/UmpsBizApplication.java
@@ -0,0 +1,13 @@
+package com.mengxj.ump.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmpsBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmpsBizApplication.class, args);
+    }
+
+}
